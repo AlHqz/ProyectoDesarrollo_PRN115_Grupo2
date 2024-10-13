06f25040450e3d0f2d34bbf9c91fa51f5c55cf2b
Raul028 <MP21057@ues.edu.sv>
2024-10-12 22:10:32 -0600

    Cambio de Raul prueba

--- Clave1_Grupo2/Form1.Designer.cs
+++ Clave1_Grupo2/Form1.Designer.cs
@@ -34,20 +34,24 @@ namespace Clave1_Grupo2
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(189, 112);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(142, 91);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(306, 105);
+            this.button1.Size = new System.Drawing.Size(230, 85);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- Clave1_Grupo2/Form1.cs
+++ Clave1_Grupo2/Form1.cs
@@ -19,7 +19,7 @@ namespace Clave1_Grupo2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola");
+            MessageBox.Show("Hola Mundo");
         }
     }
 }
